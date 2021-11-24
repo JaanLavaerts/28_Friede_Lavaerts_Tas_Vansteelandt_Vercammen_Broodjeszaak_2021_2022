4757e1c0907b5f7aafc1d4be015237226b7ef280
timvs5 <tim.vansteelandt5@gmail.com>
2021-11-24 20:14:03 +0100

    Klasse 'Bestelling' toegevoegd Klasse 'Broodje' toegevoegd

---
+++ src/model/Bestelling.java
@@ -0,0 +1,34 @@
+package model;
+
+import java.util.ArrayList;
+
+public class Bestelling {
+
+    //vars
+    private ArrayList<Broodje> bestelling;
+    //private BestellingState state;
+
+    //constructor,   public?
+    public Bestelling() {
+        this.bestelling = new ArrayList<Broodje>();
+
+    }
+
+    //getter
+    public ArrayList<Broodje> getBestelling() {
+        return (ArrayList<Broodje>) this.bestelling.clone();
+    }
+
+    //adder
+    public void addBroodje(Broodje b) {
+        this.bestelling.add(b);
+    }
+
+    //remover
+    public void removeBroodje(Broodje b) {
+        this.bestelling.remove(b);
+    }
+
+
+
+}
