8b349f9e45ba5f1a1aeca94dbb48c1e48c005212
timvs5 <tim.vansteelandt5@gmail.com>
2021-11-25 10:20:46 +0100

    Beleglader toegevoegd, getters toegevoegd, kleine aanpassing omdat ik anders niet kon pushen

--- src/model/database/BroodjesDatabase.java
+++ src/model/database/BroodjesDatabase.java
@@ -9,31 +9,85 @@ public class BroodjesDatabase {
 
     private static String FILEPATH = "./src/bestanden/";
     private HashMap<String, HashMap<String, Double>> broodjes = new HashMap<>();
+    private HashMap<String, HashMap<String, Double>> beleg = new HashMap<>();
 
+
+    //constructor
+    public BroodjesDatabase() {
+        loadBroodjes();
+        loadBeleg();
+    }
+
+
+
+    //getter
+    public double getBroodStock(String broodsoort) {
+        return this.broodjes.get(broodsoort).get("stock");
+    }
+
+    //getter
+    public double getBroodPrijs(String broodsoort) {
+        return this.broodjes.get(broodsoort).get("prijs");
+    }
+
+    //getter
+    public double getBroodVerkocht(String broodsoort) {
+        return this.broodjes.get(broodsoort).get("verkocht");
+    }
+
+    //getter
+    public double getBelegStock(String Beleg) {
+        return this.broodjes.get(Beleg).get("stock");
+    }
+
+    //getter
+    public double getBelegPrijs(String Beleg) {
+        return this.broodjes.get(Beleg).get("prijs");
+    }
+
+    //getter
+    public double getBelegVerkocht(String Beleg) {
+        return this.broodjes.get(Beleg).get("verkocht");
+    }
+
+    //getter, liefst niet gebruiken buiten deze klasse, gebruik getters
     public HashMap<String, HashMap<String, Double>> getBroodjes() {
         return broodjes;
     }
 
+    //loader
     public void loadBroodjes() {
-        try (BufferedReader reader = new BufferedReader(new FileReader(FILEPATH + "broodjes.txt"))) {
-            String line = null;
+        load("broodjes", broodjes);
+    }
+
+    //loader
+    public void loadBeleg() {
+        load("beleg", beleg);
+    }
+
+    //loads the thingy into the thingy
+    private void load(String load,  HashMap<String, HashMap<String, Double>> loadTo) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(FILEPATH + load + ".txt"))) {
+            String line;
             try {
                 line = reader.readLine();
                 while (line != null) {
                     String[] lineElements = line.split(",");
                     String naam = lineElements[0];
                     double prijs = Double.parseDouble(lineElements[1]);
+                    //waarom doubles ipv ints?
                     double stock = Double.parseDouble(lineElements[2]);
                     double verkocht = Double.parseDouble(lineElements[3]);
                     HashMap<String, Double> data = new HashMap<>();
                     data.put("prijs", prijs);
                     data.put("stock", stock);
                     data.put("verkocht", verkocht);
-                    broodjes.put(naam, data);
+                    loadTo.put(naam, data);
                     line = reader.readLine();
                 }
             } catch (IOException e) {
-
+                //geen idee wat dit doet maar anders kan ik niet pushen
+                e.printStackTrace();
             }
         } catch (IOException e) {
             e.printStackTrace();
