5f4c4a6ab6ff843024022b37e7b8f74f9e3a5481
timvs5 <tim.vansteelandt5@gmail.com>
2021-12-01 19:53:19 +0100

    Bestelling class aangepast

--- src/model/Bestelling.java
+++ src/model/Bestelling.java
@@ -1,31 +1,49 @@
 package model;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Bestelling {
 
     //vars
-    private ArrayList<Broodje> bestelling;
+    private HashMap<Broodje, Integer> bestelling;
     //private BestellingState state;
 
     //constructor,   public?
     public Bestelling() {
-        this.bestelling = new ArrayList<Broodje>();
+        this.bestelling = new HashMap<Broodje, Integer>();
     }
 
     //getter
-    public ArrayList<Broodje> getBestelling() {
-        return (ArrayList<Broodje>) this.bestelling.clone();
+    public HashMap<Broodje, Integer> getBestelling() {
+        return (HashMap<Broodje, Integer>) this.bestelling.clone();
+    }
+
+    //adder
+    public void addBroodje(Broodje b, int aantal) {
+        int amount = getBroodjeAmount(b) + aantal;
+        this.bestelling.put(b, amount);
     }
 
     //adder
     public void addBroodje(Broodje b) {
-        this.bestelling.add(b);
+        addBroodje(b, 1);
+    }
+
+    //getter
+    public int getBroodjeAmount(Broodje b) {
+        if (this.bestelling.get(b) != null) {
+            return 0;
+        }
+        return this.bestelling.get(b);
     }
 
     //remover
-    public void removeBroodje(Broodje b) {
-        this.bestelling.remove(b);
+    public void removeBroodje(Broodje b, int aantal) {
+        addBroodje(b, -aantal);
+        if (getBroodjeAmount(b) < 1) {
+            this.bestelling.remove(b);
+        }
     }
 
 
