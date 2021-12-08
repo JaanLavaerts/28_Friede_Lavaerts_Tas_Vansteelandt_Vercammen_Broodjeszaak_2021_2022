18d1979062db266e663919fa83af8546437b31b4
Jaan Lavaerts <jaan.lavaerts@student.ucll.be>
2021-12-08 17:53:54 +0100

    story 2

---
+++ src/model/database/LoadSaveStrategies/Text/BelegTekstLoadSaveStrategy.java
@@ -0,0 +1,25 @@
+package model.database.LoadSaveStrategies.Text;
+
+import model.BelegSoort;
+import model.database.LoadSaveStrategies.LoadSaveStrategy;
+import utilities.TekstLoadSaveTemplate;
+
+import java.util.HashMap;
+
+public class BelegTekstLoadSaveStrategy extends TekstLoadSaveTemplate{
+
+    @Override
+    public HashMap<String, Object> load() {
+        return load("beleg");
+    }
+
+    @Override
+    public void save(HashMap<String, Object> data) {
+
+    }
+
+    protected Object FormatData(String naam, double prijs, int stock, int verkocht){
+        return new BelegSoort(naam, prijs, stock, verkocht);
+    }
+
+}
